474a3efecea22473e212c5a1f655c3c33f8f6e32
ElhamMir <mireshghi@yahoo.com>
2021-12-07 20:10:25 -0500

    incorporate DayList into application

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "index.scss";
+import DayList from "components/DayList.js"
 
 import Application from "components/Application";
 
 ReactDOM.render(<Application />, document.getElementById("root"));
+ 
