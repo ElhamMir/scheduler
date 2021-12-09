0525df72486cfc042b08303b17076cd45432f3bc
ElhamMir <mireshghi@yahoo.com>
2021-12-08 22:47:31 -0500

    changed list to controlled list

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -6,12 +6,14 @@ export default function DayList(props) {
     const availableDays = props.days.map(day => {
       return (
         <DayListItem
-          key={day.id}
-          name={day.name}
-          spots={day.spots}
-          selected={day.name === props.day}
-          setDay={props.setDay}
-        />
+      key={day.id} 
+      name={day.name} 
+      spots={day.spots} 
+      selected={day.name === day.value}
+      setDay={day.onChange}
+    />
+
+
       );
     });
   
