3d190a751bfbcb965fc43ece2b69f837c7870db0
ElhamMir <mireshghi@yahoo.com>
2021-12-12 22:38:53 -0500

    fix

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import "components/InterviewerListItem.scss";
 import classNames from "classnames";
--- stories/index.js
+++ stories/index.js
@@ -12,15 +12,17 @@ import Button from "components/Button";
 import DayList from "components/DayList.js"
 import InterviewerListItem from "components/InterviewerListItem.js";
  import InterviewerList from "components/InterviewerList.js";
-// import Appointment from "components/Appointment/index.js";
-// import Header from "components/Appointment/Header.js"
-// import Show from "components/Appointment/Show.js"
-// import Empty from "components/Appointment/Empty.js"
-// import Confirm from "components/Appointment/Confirm.js"
-// import Status
-// from "components/Appointment/Status.js"
-// import Error
-// from "components/Appointment/Error.js"
+ import Appointment from "components/Appointment/index.js";
+
+//import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header.js"
+import Show from "components/Appointment/Show.js"
+import Empty from "components/Appointment/Empty.js"
+import Confirm from "components/Appointment/Confirm.js"
+import Status
+from "components/Appointment/Status.js"
+import Error
+from "components/Appointment/Error.js"
 // import Form from "components/Appointment/Form.js";
 
 storiesOf("Button", module)
@@ -139,8 +141,49 @@ storiesOf("Button", module)
           interviewers={interviewers}
           onChange={action("setInterviewer")}
         />
-  ));
-     
+  ))
+  storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />);
+  storiesOf("Appointment", module)
+      .addParameters({
+        backgrounds: [{ name: "white", value: "#fff", default: true }]
+      })
+      .add("Appointment", () => <Appointment />)
+      .add("Appointment with Time", () => <Appointment time="12pm" />);
+    
+  
+      storiesOf("Appointment", module)
+    .addParameters({
+      backgrounds: [{ name: "white", value: "#fff", default: true }]
+      
+    })
+    .add("Appointment", () => <Appointment />)
+    .add("Appointment with Time", () => <Appointment time="12pm" />)
+    .add("Header", () => <Header time="12pm" />)
+    .add('Show', () => (
+      <Show
+        student="Lydia Miller-Jones"
+        interviewer={interviewers[0]}
+        onEdit={action('onEdit')}
+        onDelete={action('onDelete')}
+      />
+    ))
+    .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+    .add('Confirm', () => (
+      <Confirm
+        message="Delete the appointment?"
+        onConfirm={action('onConfirm')}
+        onCancel={action('onCancel')}
+      />
+    ))
+    .add('Status', () => <Status message="Deleting" />)
+    .add('Error', () => (
+      <Error message="Could not save appointment" onClose={action('onClose')} />
+    ))
 //   storiesOf("DayListItem", module) //Initiates Storybook and registers our DayListItem component
 //   .addParameters({
 //     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
