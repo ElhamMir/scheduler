3d155d40555010f89a77240813589944851339e4
ElhamMir <mireshghi@yahoo.com>
2021-12-13 19:05:08 -0500

    add appointment to app

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,12 @@
 //import React from "react";
+import Appointment from "components/Appointment"
 import DayList from "components/DayList.js"
 import "components/Application.scss";
 import React, { useState } from "react";
-
+import Empty from "components/Appointment/Empty.js"
+import Header from "components/Appointment/Header.js"
+import Show from "components/Appointment/Show.js"
+//Import components/Appointment into Application.js
 const days = [
   {
     id: 1,
@@ -20,9 +24,62 @@ const days = [
     spots: 0,
   },
 ];
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const schedule = appointments.map(appointment => { if(appointment.interview)
+    return  <Show
+    student={appointment.interview.student}
+    interviewer={appointment.interview}
+   
+  />
+return (<Header time={"12pm"} />)
+    {/* // return <Appointment key={appointment.id} id={appointment.id} time={appointment.time} interview={appointment.interview}  /> */}
+    
+    ;
+  });
+    
 
+   
+  
   return (
     <main className="layout">
     
@@ -43,11 +100,13 @@ export default function Application(props) {
   src="images/lhl.png"
   alt="Lighthouse Labs"
 />
+{/* <Appointment key={appointment.id} id={appointment.id} time={appointment.time} interview={appointment.interview} /> */}
 
         {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {schedule}
+       
       </section>
       
     </main>
