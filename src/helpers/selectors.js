5f6d243d8c62726ef5a102acb185c3288ef0f28c
ElhamMir <mireshghi@yahoo.com>
2021-12-14 20:57:16 -0500

    add getAppointmentsForDay method

---
+++ src/helpers/selectors.js
@@ -0,0 +1,28 @@
+export function getAppointmentsForDay(state, day) {
+    //... returns an array of appointments for that day
+    console.log("kkk",state.days.map(day => day.name))
+    let results=[]
+    console.log(state.days,"state.days")
+    console.log(day,"day")
+    let flag = false
+    for(let i = 0; i<state.days.length; i++){
+        
+        console.log(state.days[i])
+        if(state.days[i]["name"] === day){
+            flag = true
+            results=[]
+            //console.log('state.days[i]["appointment"]',state.days[i].appointments)
+            for(let j of state.days[i].appointments){
+                results.push(state.appointments[j])
+            }
+            console.log(results,"restuszrgzsrhbf")
+          
+        }
+    }
+    if(flag === false) {
+        return []
+    }
+    return results
+
+  }
+  
