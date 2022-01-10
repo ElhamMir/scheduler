2ce018e660fef6d40bbe577261ea286546a3299c
ElhamMir <mireshghi@yahoo.com>
2022-01-09 20:48:06 -0500

    fix dayList

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -8,6 +8,7 @@
 */
 import React, { useState,useEffect } from "react";
 import axios from "axios";
+import DayListItem from "components/DayListItem";
 
 export default function useApplicationData () {
     const [state, setState] = useState({
@@ -35,11 +36,15 @@ export default function useApplicationData () {
           ...state,
           appointments
         });
+        console.log("daylistitem is here", DayListItem)
         return axios.put(`/api/appointments/${id}`, appointment)
         .then(response => {
           if (response.status === 204) setState({...state, appointments});
+        //   DayListItem.spots +=1
         })
+        
       }
+      
       function cancelInterview(id) {
         const appointment = {
           ...state.appointments[id],
